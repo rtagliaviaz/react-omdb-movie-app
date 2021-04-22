9cdd4b60d436da6b408a9e7b8537f0939df7abb8
rtagliavia <rtagliavia.pp@gmail.com>
2021-04-22 14:05:58 -0400

    'first'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import {Link,BrowserRouter, Route, Switch } from 'react-router-dom'
+import {BrowserRouter, Route, Switch } from 'react-router-dom'
 import { HashRouter, Route } from 'react-router-dom'
 //components
 import Home from './components/Home'
@@ -8,16 +8,16 @@ import Movie from './components/Movie'
 const App = () => {
   return (
       <div className="App">
-        {/* <BrowserRouter> */}
-        <HashRouter >
+        <BrowserRouter>
+        {/* <HashRouter > */}
           {/* <Switch> */}
           <>
             <Route exact path='/' component={Home}/>
             <Route exact path='/details/:imdbId' component={Movie}/>
           </>
           {/* </Switch> */}
-        </HashRouter>
-        {/* </BrowserRouter> */}
+        {/* </HashRouter> */}
+        </BrowserRouter>
       </div>
   );
 }
